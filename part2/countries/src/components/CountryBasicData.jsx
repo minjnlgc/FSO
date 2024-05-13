2987d12fbc6da6d9ef7b132f3948f1e06e1aaa61
minjnlgc <kunyuquemian@outlook.com>
2024-05-13 14:12:18 +0100

    improved part2 e countriesdata

--- part2/countries/src/components/CountryBasicData.jsx
+++ part2/countries/src/components/CountryBasicData.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import weatherService from "../services/weather";
+import { useEffect } from "react";
 
 const CountryBasicData = ({ country }) => {
   const [temp, setTemp] = useState(0);
@@ -8,11 +9,16 @@ const CountryBasicData = ({ country }) => {
 
   const [lat, lon] = Object.values(country.latlng);
 
-  weatherService.getByLatAndLon(lat, lon).then((data) => {
-    setTemp(data.current.temp);
-    setWindSpeed(data.current.wind_speed);
-    setIconId(data.current.weather[0].icon);
-  });
+  useEffect(() => {
+    weatherService
+      .getByLatAndLon(lat, lon)
+      .then((data) => {
+        setTemp(data.current.temp);
+        setWindSpeed(data.current.wind_speed);
+        setIconId(data.current.weather[0].icon);
+      })
+      .catch((error) => console.log("failed to fetch weather data!"));
+  }, []);
 
   return (
     <>
