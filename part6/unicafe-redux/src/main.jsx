efeb6c31b37124e66931aa8d72a46b8746675f6a
minjnlgc <kunyuquemian@outlook.com>
2024-05-23 18:45:50 +0100

    part6a 6.1-6.2

---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import "./index.css";
+import "./App.css";
+
+import counterReducer from "./reducers/counterReducer";
+
+const store = createStore(counterReducer);
+
+const App = () => {
+  const handleGood = () => {
+    store.dispatch({
+      type: "GOOD",
+    });
+  };
+
+  const handleOk = () => {
+    store.dispatch({
+      type: "OK",
+    });
+  };
+
+  const handleBad = () => {
+    store.dispatch({
+      type: "BAD",
+    });
+  };
+
+  const handleZero = () => {
+    store.dispatch({
+      type: "ZERO",
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleOk}>ok</button>
+      <button onClick={handleBad}>bad</button>
+      <button onClick={handleZero}>reset stats</button>
+      <div> good {store.getState().good} </div>
+      <div> ok {store.getState().ok} </div>
+      <div> bad {store.getState().bad} </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
---
+++ part6/unicafe-redux/src/reducers/counterReducer.test.js
@@ -0,0 +1,86 @@
+import counterReducer from "./counterReducer";
+import deepFreeze from "deep-freeze";
+
+describe("counterReducer", () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0,
+  };
+
+  test("should return a proper initial state when called with undefined state", () => {
+    const action = {
+      type: "DO_NOTHING",
+    };
+
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  test("good is incremented", () => {
+    const stateAtStart = {
+      good: 2,
+      ok: 4,
+      bad: 5,
+    };
+    const action = {
+      type: "GOOD",
+    };
+
+    deepFreeze(stateAtStart);
+
+    const newState = counterReducer(stateAtStart, action);
+    expect(newState).toEqual({ ...stateAtStart, good: stateAtStart.good + 1 });
+  });
+
+  test("bad is decreased", () => {
+    const stateAtStart = {
+      good: 2,
+      ok: 4,
+      bad: 5,
+    };
+
+    const action = {
+      type: "BAD",
+    };
+
+    deepFreeze(stateAtStart);
+
+    const newState = counterReducer(stateAtStart, action);
+    expect(newState).toEqual({ ...stateAtStart, bad: stateAtStart.bad + 1 });
+  });
+
+  test("ok is incremented", () => {
+    const stateAtStart = {
+        good: 2,
+        ok: 4,
+        bad: 5,
+      };
+
+    const action = {
+      type: "OK",
+    };
+
+    deepFreeze(stateAtStart);
+
+    const newState = counterReducer(stateAtStart, action);
+    expect(newState).toEqual({...stateAtStart, ok: stateAtStart.ok + 1});
+  });
+
+  test("zero is reset all to zero", () => {
+    const stateAtStart = {
+      good: 10,
+      ok: 10,
+      bad: 10,
+    };
+
+    const action = {
+      type: "ZERO",
+    };
+
+    deepFreeze(stateAtStart);
+
+    const newState = counterReducer(stateAtStart, action);
+    expect(newState).toEqual(initialState);
+  });
+});
