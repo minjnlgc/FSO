bbd1af63daaac125f88cfcd08f0508cb5e21a3ab
minjnlgc <kunyuquemian@outlook.com>
2024-06-16 22:09:54 +0100

    part9e form

--- part9/patientor/src/components/AddEntryForm/HealthCheckEntryForm.tsx
+++ part9/patientor/src/components/AddEntryForm/HealthCheckEntryForm.tsx
@@ -1,5 +1,81 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Input,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import { HealthCheckRating } from "../../types";
+
 const HealthCheckEntryForm = () => {
-    
-}
+  const handleSubmit = () => {
+    console.log("submit!");
+  };
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom>
+          <strong>New HealthCheck entry</strong>
+        </Typography>
+
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="description"
+            fullWidth
+            label="Description"
+            multiline
+            maxRows={4}
+          />
+
+          <Input type="date" sx={{ mb: 1.5, mt: 2.5, ml: 1 }} />
+
+          <TextField
+            id="specialist"
+            label="Specialist"
+            fullWidth
+            sx={{ mb: 2, mt: 2 }}
+          />
+
+          <TextField
+            id="healthcheckrating"
+            select
+            label="Health check rating"
+            defaultValue=""
+            fullWidth
+            sx={{ mb: 2, mt: 2 }}
+          >
+            {Object.keys(HealthCheckRating)
+              .filter((key) => isNaN(Number(key))) // Filters out numeric keys
+              .map((key) => {
+                const value =
+                  HealthCheckRating[key as keyof typeof HealthCheckRating];
+                return (
+                  <MenuItem key={value} value={value}>
+                    {key}
+                  </MenuItem>
+                );
+              })}
+          </TextField>
+
+          {/* diagnosis codes */}
+          <TextField
+            id="diagnosisCodes"
+            label="Diagnosis Codes"
+            fullWidth
+            sx={{ mb: 2, mt: 2 }}
+          />
+
+          <Button variant="contained" color="error">
+            Cancel
+          </Button>
+          <Button type="submit">Add</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default HealthCheckEntryForm;
--- part9/patientor/src/components/AddEntryForm/index.tsx
+++ part9/patientor/src/components/AddEntryForm/index.tsx
@@ -1,37 +1,11 @@
-import { Card, CardContent, Input, TextField, Typography } from "@mui/material";
-import diagnoses from "../../services/diagnoses";
+import HealthCheckEntryForm from "./HealthCheckEntryForm";
 
 const AddEntryForm = () => {
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography gutterBottom>
-          <strong>New HealthCheck entry</strong>
-        </Typography>
-        {/* description */}
-        <TextField
-          id="description"
-          fullWidth
-          label="description"
-          multiline
-          maxRows={4}
-        />
-
-        {/* date */}
-        <Input type="date" />
-
-        {/* specialist */}
-        <TextField
-            id="specialist"
-            label="specialist"
-            fullWidth
-        />
-
-        {/* healthcheckrating */}
-        {/* diagnosis codes */}
-      </CardContent>
-    </Card>
-  );
+    <div>
+        <HealthCheckEntryForm />
+    </div>
+  )
 };
 
 export default AddEntryForm;
